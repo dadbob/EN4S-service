278758226e862c952689e741b444c718cde95880
jeffisabelle <muhitosan@gmail.com>
2013-06-08 21:33:24 +0300

    initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# configuration
+HOST = "0.0.0.0"
+PORT = 5000
+DEBUG = True
+SECRET = "somesecretkeyhere"
+CLIENT = MongoClient('localhost', 27017)
+db = CLIENT.en4s
